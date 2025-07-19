5380ab7da17a836691e9985e744863fe7a51ece5
fathan751 <fathanq641@gmail.com>
2025-07-19 08:34:28 +0700

    fix warning

--- src/pages/TopUserDetailPage.jsx
+++ src/pages/TopUserDetailPage.jsx
@@ -36,7 +36,7 @@ const TopUserDetailPage = () => {
             </Link>
             <div className="ml-20 mt-20 flex lg:flex-row flex-col h-fit items-center border-2 gap-10 w-fit">
                 <img src={users?.avatar} alt="logo" className="border-r-2 lg:w-fit"></img>
-                <p className="text-xl lg:border-r-2 sm:border-b-2 lg:border-b-0 border-t-2 lg:border-t-0 w-full lg:w-fit h-[128px] justify-center items-center flex pr-5 w-fit">user id = {id}</p>
+                <p className="text-xl lg:border-r-2 sm:border-b-2 lg:border-b-0 border-t-2 lg:border-t-0 w-full lg:w-fit h-[128px] justify-center items-center flex pr-5">user id = {id}</p>
                 <p className="text-xl lg:border-r-2 sm:border-b-2 lg:border-b-0 w-full lg:w-fit h-[128px] justify-center items-center flex pr-5">Nama Depan : {users.first_name} </p>
                 <p className="text-xl lg:border-r-2 sm:border-b-2 lg:border-b-0 w-full lg:w-fit h-[128px] justify-center items-center flex pr-5">Nama Belakang : {users.last_name} </p>
                 <p className="text-xl lg:border-r-2 sm:border-b-2 lg:border-b-0 w-full lg:w-fit h-[128px] justify-center items-center flex pr-5">email : {users.email}</p>
